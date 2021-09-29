7de390ad4e5a7b30a5e0e07c27a7e65743cc2f98
Kian Sweeney <73000700+kiansweeney11@users.noreply.github.com>
2021-09-29 17:10:56 +0100

    builds

--- _build/jupyter_execute/clustering.py
+++ _build/jupyter_execute/clustering.py
@@ -3,6 +3,7 @@
 
 # # Clustering
 # ## After our analysis of the data we must now cluster the data accordingly
+# Importing approriate libraries
 
 # In[1]:
 
@@ -13,6 +14,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.metrics import silhouette_score
+
+
+# ### First, we read in our data
+
+# In[2]:
+
+
+index95 = pd.read_csv('data/index_95.csv')
+index100 = pd.read_csv('data/index_100.csv')
+index150 = pd.read_csv('data/index_150.csv')
+win95 = pd.read_csv('data/wi_95.csv')
+win100 = pd.read_csv('data/wi_100.csv')
+win150 = pd.read_csv('data/wi_150.csv')
+loss95 = pd.read_csv('data/lo_95.csv')
+loss100 = pd.read_csv('data/lo_100.csv')
+loss150 = pd.read_csv('data/lo_150.csv')
+choice95 = pd.read_csv('data/choice_95.csv')
+choice100 = pd.read_csv('data/choice_100.csv')
+choice150 = pd.read_csv('data/choice_150.csv')
 
 
 # In[ ]:
--- _build/jupyter_execute/introduction.py
+++ _build/jupyter_execute/introduction.py
@@ -6,11 +6,11 @@
 # ## Iowa Gambling Task
 # The [Iowa Gambling Task](https://www.youtube.com/watch?v=A6SsQIyJMhs)(IGT) is a pyscological card game thought to portray real world decision making and help understand how and why people make decisions. Typically, the particpiants start with a loan of $2000 and are typically given a deck of four cards to pick from. The deck either rewards or punishes the particpiant each time and the end goal of the game is to make more money than what they started with. 
 # 
-# ![gambling-task.jpg](attachment:gambling-task.jpg)
+# <img src="images/gambling-task.jpg" alt="Gambling Task Pic"/>
 
 # There is typically 100 runs of the experiment and the decks are set up in a way that some reward the particpiant over time while others negatively impact the user over time. The decks that affect the user over time tend to have big rewards from time to time while at the same time lose the user money if they go for that deck regularly. Other decks then use smaller rewards and a more risk averse method of playing. 
 # 
-# ![Payoff-scheme-of-the-Iowa-Gambling-Task.png](attachment:Payoff-scheme-of-the-Iowa-Gambling-Task.png)
+# <img src="images/Payoff-scheme-of-the-Iowa-Gambling-Task.png" alt="Payoff Scheme Pic"/>
 
 # In[ ]:
 
