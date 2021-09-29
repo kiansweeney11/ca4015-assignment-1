234a8fa542070d7d229b2b4ea5c13c2ce7b393b7
Kian Sweeney <73000700+kiansweeney11@users.noreply.github.com>
2021-09-29 20:43:02 +0100

    toc

--- _build/jupyter_execute/introduction.py
+++ _build/jupyter_execute/introduction.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Introduction - CA4015 First Assignment
+# # CA4015 Assignment 1
 # 
-# ## Iowa Gambling Task
-# The [Iowa Gambling Task](https://www.youtube.com/watch?v=A6SsQIyJMhs)(IGT) is a pyscological card game thought to portray real world decision making and help understand how and why people make decisions. Typically, the particpiants start with a loan of $2000 and are typically given a deck of four cards to pick from. The deck either rewards or punishes the particpiant each time and the end goal of the game is to make more money than what they started with. 
+# ## Introduction
 # 
+# ### Iowa Gambling Task¶
+# The [Iowa Gambling Task](https://www.youtube.com/watch?v=A6SsQIyJMhs)(IGT) is a pyscological card game thought to portray real world decision making and help understand how and why people make decisions. Typically, the particpiants start with a loan of $2000 and are typically given a deck of four cards to pick from. The deck either rewards or punishes the particpiant each time and the end goal of the game is to make more money than what they started with. Some of the cards are more favourable than others, providing a steady winning return over the long term whereas others can be typically destructive but sporadically produce high winnings. 
 # 
 # ![](https://media.imotions.com/images/20190528132235/The-Iowa-Gambling-Task-%E2%80%93-No-Dice-All-Science.jpg)
-
+# 
 # ### Dataset
 # For the purpose of this assignment there is varying trial lengths for each task. Typically, the task is run in 100 trials but in this instance we have a 95 trial, 100 trial and 150 trial experiments. We were given a variety of data for the trials. There were 617 particpiants across all the studies and all were reported as "healthy". We had each subjects choice on their respective trial, their winnings on the respective rounds combined with their losses for each round. We also had an "index" file which told us which study the subject was part of. An overview of our data and other information such as the particpiant demographics can be seen [here](https://openpsychologydata.metajnl.com/articles/10.5334/jopd.ak/).
 # 
