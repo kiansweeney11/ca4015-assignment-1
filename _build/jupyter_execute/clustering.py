0f5474c8203e59b7f4aa6c69b87cf5e78766d435
Kian Sweeney <73000700+kiansweeney11@users.noreply.github.com>
2021-10-02 15:00:17 +0100

    study-cluster-comp

--- _build/jupyter_execute/clustering.py
+++ _build/jupyter_execute/clustering.py
@@ -53,18 +53,18 @@ cleaned95.head()
 
 # Initially, I decided to cluster based on the profit/loss margin for each subject and their most common choice. However, the most common choice would limit the clusters greatly I felt. There would only be 4 possible values (1,2,3 or 4) and this would limit what we could learn from the approriate cluster analysis. This lead me to going back to my data processing and creating the average choice column to add to my data. This was the sum of all the subjects selection divided by the number of trials and I felt this would provide better cluster analysis as a result as there would be far more variety in the range of values.
 
-# In[100]:
+# In[123]:
 
 
 kmeans_margin_choice = KMeans(n_clusters=3).fit(cleaned95[["Margin", "Average Choice"]])
 centroids_betas = kmeans_margin_choice.cluster_centers_
 
 
-# In[101]:
+# In[124]:
 
 
 plt.scatter(cleaned95['Margin'], cleaned95['Average Choice'], c= kmeans_margin_choice.labels_, cmap = "Set1", alpha=0.5)
-plt.scatter(centroids_betas[:, 0], centroids_betas[:, 1], c='blue')
+plt.scatter(centroids_betas[:, 0], centroids_betas[:, 1], c='blue', marker='x')
 plt.title('K-Means cluster for 95 Trial Subjects')
 plt.xlabel('Profit/Loss Margin')
 plt.ylabel('Average Choice')
@@ -73,18 +73,18 @@ plt.show()
 
 # From our cluster analysis here of the 95 trial experiments, it is interesting to note in the Fridberg study the cluster which had the highest average choice also made the most money by a significant distance. K = 3 is certainly the optimal number of clusters here as they are very much pre-defined and easy to distinguish by looking at the scatter plot.
 
-# In[102]:
+# In[108]:
 
 
 kmeans_margin_choice100 = KMeans(n_clusters=3).fit(cleaned100[["Margin", "Average Choice"]])
 centroids_betas = kmeans_margin_choice100.cluster_centers_
 
 
-# In[103]:
+# In[109]:
 
 
 plt.scatter(cleaned100['Margin'], cleaned100['Average Choice'], c= kmeans_margin_choice100.labels_, cmap = "Set1", alpha=0.5)
-plt.scatter(centroids_betas[:, 0], centroids_betas[:, 1], c='blue')
+plt.scatter(centroids_betas[:, 0], centroids_betas[:, 1], c='blue', marker='*')
 plt.title('K-Means cluster for 100 Trial Subjects')
 plt.xlabel('Margin')
 plt.ylabel('Average Choice')
@@ -93,18 +93,18 @@ plt.show()
 
 # Again
 
-# In[104]:
+# In[106]:
 
 
 kmeans_margin_choice150 = KMeans(n_clusters=3).fit(cleaned150[["Margin", "Average Choice"]])
 centroids_betas = kmeans_margin_choice150.cluster_centers_
 
 
-# In[105]:
+# In[107]:
 
 
 plt.scatter(cleaned150['Margin'], cleaned150['Average Choice'], c= kmeans_margin_choice150.labels_, cmap = "Set1", alpha=0.5)
-plt.scatter(centroids_betas[:, 0], centroids_betas[:, 1], c='blue')
+plt.scatter(centroids_betas[:, 0], centroids_betas[:, 1], c='blue', marker='x')
 plt.title('K-Means cluster for 150 Trial Subjects')
 plt.xlabel('Margin')
 plt.ylabel('Choice')
@@ -133,29 +133,30 @@ plt.title('The Elbow Method showing the optimal k for 100 Trial Dataset')
 plt.show()
 
 
-# From the elbow method graph above, we can see that the optimal number of clusters is K=2 or K=3 for the 100 trial dataset. 
+# From the elbow method graph above, we can see that the optimal number of clusters is K=3 for the 100 trial dataset. 
 
-# In[96]:
+# In[119]:
 
 
-distortion = []
-N = range(1,10)
-for k in N:
+sse_values = []
+Z = range(1,10)
+for k in Z:
     kmeanModel = KMeans(n_clusters=k)
     kmeanModel.fit(cleaned150[['Margin', 'Average Choice', 'Most Common Choice']])
-    distortion.append(kmeanModel.inertia_)
+    sse_values.append(kmeanModel.inertia_)
 
 
-# In[97]:
+# In[120]:
 
 
-plt.figure(figsize=(15,8))
-plt.plot(N, distortions, 'bx-')
+plt.figure(figsize=(16,8))
 plt.xlabel('k')
-plt.ylabel('Distortion')
+plt.ylabel('SSE')
 plt.title('The Elbow Method showing the optimal k for 150 Trial Dataset')
-plt.show()
+plt.plot(N, sse)
 
+
+# Again, we can see that the optimal number of clusters for our 150 trial dataset is K=3.
 
 # In[ ]:
 
