94479e7b2da574ceb0b85648159c316b13c33c9e
Kian Sweeney <73000700+kiansweeney11@users.noreply.github.com>
2021-10-02 13:47:59 +0100

    tidy-cluster

---
+++ _build/jupyter_execute/k-means_variation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import seaborn as sn
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from sklearn.metrics import silhouette_score
+
+
+# In[ ]:
+
+
+index95 = pd.read_csv('data/index_95.csv')
+index100 = pd.read_csv('data/index_100.csv')
+index150 = pd.read_csv('data/index_150.csv')
+win95 = pd.read_csv('data/wi_95.csv')
+win100 = pd.read_csv('data/wi_100.csv')
+win150 = pd.read_csv('data/wi_150.csv')
+loss95 = pd.read_csv('data/lo_95.csv')
+loss100 = pd.read_csv('data/lo_100.csv')
+loss150 = pd.read_csv('data/lo_150.csv')
+choice95 = pd.read_csv('data/choice_95.csv')
+choice100 = pd.read_csv('data/choice_100.csv')
+choice150 = pd.read_csv('data/choice_150.csv')
+
+cleaned95 = pd.read_csv('data/cleaned95.csv', index_col='Unnamed: 0')
+cleaned100 = pd.read_csv('data/cleaned100.csv', index_col='Unnamed: 0')
+cleaned150 = pd.read_csv('data/cleaned150.csv', index_col='Unnamed: 0')
+
