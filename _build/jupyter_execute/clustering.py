52672b792c230d3afc857335d0ee856bb7189551
Kian Sweeney <73000700+kiansweeney11@users.noreply.github.com>
2021-10-08 17:45:23 +0100

    small-changes

--- _build/jupyter_execute/clustering.py
+++ _build/jupyter_execute/clustering.py
@@ -46,7 +46,7 @@ cleaned100 = pd.read_csv('data/cleaned100.csv', index_col='Unnamed: 0')
 cleaned150 = pd.read_csv('data/cleaned150.csv', index_col='Unnamed: 0')
 
 
-# Initially, I decided to cluster based on the profit/loss margin for each subject and their most common choice. However, the most common choice would limit the clusters greatly I felt. There would only be 4 possible values (1,2,3 or 4) and this would limit what we could learn from the approriate cluster analysis. I looked into clustering on the number of times each deck was selected but this would involve multiple clusters, one for each choice against the profit margin but I decided against it. This lead me to going back to my data processing and creating the average choice column to add to my data. This was the sum of all the subjects selection divided by the number of trials and I felt this would provide better cluster analysis as a result as there would be far more variety in the range of values. I also decided to look into how many times subjects picked their most common choice. I felt this would give us a good overview of the respective studies and how they played the game, did they play safe and stick to what they know would win or would they attempt riskier decks in the hope of winning more?
+# Initially, I decided to cluster based on the profit/loss margin for each subject and their most common choice. However, the most common choice would limit the clusters greatly I felt. There would only be 4 possible values (1,2,3 or 4) and this would limit what we could learn from the approriate cluster analysis. I looked into clustering on the number of times each deck was selected but this would involve multiple clusters, one for each choice against the profit margin but I decided against it. This lead me to going back to my data processing and creating the average choice column to add to my data. This was the sum of all the subjects selection divided by the number of trials and I felt this would provide better cluster analysis as a result as there would be far more variety in the range of values. I also decided to look into how many times subjects picked their most common choice. I felt this would give us a good overview of the respective studies and how they played the game, did they play safe and stick to what they know would win or would they attempt riskier decks in the hope of winning more? We are going to use our standardized data from our data processing as standardized data tends to work better with the k-means algorithm.
 
 # # K-Means analysis 
 
@@ -60,26 +60,26 @@ cleaned150 = pd.read_csv('data/cleaned150.csv', index_col='Unnamed: 0')
 # 
 # In the formula above from [here](https://towardsdatascience.com/unsupervised-learning-techniques-using-python-k-means-and-silhouette-score-for-clustering-d6dd1f30b660) bi represents the shortest mean distance between a point to all points in any other cluster of which i is not a part whereas ai is the mean distance of i and all data points from the same cluster.
 
-# In[56]:
+# In[171]:
 
 
-joined = pd.read_csv('Data/cleaned_all.csv', index_col='Unnamed: 0')
-joined.head()
+standard = pd.read_csv('data/standardized_all.csv', index_col='Unnamed: 0')
+standard.head()
 
 
-# In[57]:
+# In[172]:
 
 
 for n in range(2, 11):
-    km = KMeans(n_clusters=n, random_state=42)
+    km = KMeans(n_clusters=n)
 #
 # Fit the KMeans model
 # Have to pick subset of columns as Study column is in string format
-    km.fit_predict(joined)
+    km.fit_predict(standard)
 #
 # Calculate Silhoutte Score
 #
-    score = silhouette_score(joined, km.labels_, metric='euclidean')
+    score = silhouette_score(standard, km.labels_, metric='euclidean')
 #
 # Print the score
 #
@@ -88,21 +88,21 @@ for n in range(2, 11):
 
 # #### Elbow Method
 
-# In[62]:
+# In[173]:
 
 
-distortions_joined = []
+distortions_joined_st = []
 for k in K:
     kmeanModel = KMeans(n_clusters=k)
-    kmeanModel.fit(joined)
-    distortions_joined.append(kmeanModel.inertia_)
+    kmeanModel.fit(standard)
+    distortions_joined_st.append(kmeanModel.inertia_)
 
 
-# In[63]:
+# In[183]:
 
 
 plt.figure(figsize=(16,8))
-plt.plot(K, distortions, 'bx-')
+plt.plot(K, distortions_joined_st, 'bx-')
 plt.xlabel('k')
 plt.ylabel('Distortion')
 plt.title('The Elbow Method showing the optimal k for whole Dataset')
@@ -113,40 +113,40 @@ plt.show()
 
 # ## Now we look at what we will cluster on in our data
 
-# In[58]:
+# In[175]:
 
 
-pd.plotting.scatter_matrix(joined, figsize=(10,10), hist_kwds=dict(bins=50), cmap="Set1")
+pd.plotting.scatter_matrix(standard, figsize=(10,10), hist_kwds=dict(bins=50), cmap="Set1")
 plt.show()
 
 
 # After looking at this scatterplot comparing the various columns against each other in a scatter plot I felt it might be worth looking into how regularly a participant picked their most common choice and the profit margins. We could further look into this by looking at what was their most common choice and the respective study they belonged to.
 
-# In[83]:
+# In[176]:
 
 
-kmeans_margin_joined = KMeans(n_clusters=3).fit(joined[["Margin", "Most Common Choice Picked"]])
-centroids_betas_joined = kmeans_margin_joined.cluster_centers_
+kmeans_margin_standard = KMeans(n_clusters=3).fit(standard[["Margin", "Most Common Choice Picked"]])
+centroids_betas_standard = kmeans_margin_standard.cluster_centers_
 
 
-# In[84]:
+# In[177]:
 
 
 plt.figure(figsize=(16,8))
-plt.scatter(joined['Margin'], joined['Most Common Choice Picked'], c= kmeans_margin_joined.labels_, cmap = "Set1", alpha=0.5)
-plt.scatter(centroids_betas_joined[:, 0], centroids_betas_joined[:, 1], c='blue', marker='x')
+plt.scatter(standard['Margin'], standard['Most Common Choice Picked'], c= kmeans_margin_standard.labels_, cmap = "Set1", alpha=0.5)
+plt.scatter(centroids_betas_standard[:, 0], centroids_betas_standard[:, 1], c='blue', marker='x')
 plt.title('K-Means cluster for all Subjects - Most Common Choice')
 plt.xlabel('Margin')
 plt.ylabel('Times Most Common Choice Picked')
 plt.show()
 
 
-# In[78]:
+# In[182]:
 
 
 plt.figure(figsize=(16,8))
-plt.scatter(joined['Margin'], joined['Most Common Choice Picked'], c=joined['StudyNumber'], cmap='tab10')
-plt.title("Study Clusters")
+plt.scatter(standard['Margin'], standard['Most Common Choice Picked'], c=standard['StudyNumber'], cmap='tab10')
+plt.title("Most common choice picked - Study groups")
 plt.xlabel('Margin')
 plt.ylabel('Times most common choice selected')
 plt.colorbar()
@@ -156,26 +156,26 @@ plt.colorbar()
 
 # ## Next we look at the clusters involving a subject's average choice and the profit / loss margin they made.
 
-# In[79]:
+# In[179]:
 
 
-kmeans_margin_average = KMeans(n_clusters=3).fit(joined[["Margin", "Average Choice"]])
-centroids_betas_average = kmeans_margin_average.cluster_centers_
+kmeans_margin_average_st = KMeans(n_clusters=3).fit(standard[["Margin", "Average Choice"]])
+centroids_betas_average_st = kmeans_margin_average_st.cluster_centers_
 
 
-# In[81]:
+# In[180]:
 
 
 plt.figure(figsize=(16,8))
-plt.scatter(joined['Margin'], joined['Average Choice'], c= kmeans_margin_average.labels_, cmap = "Set1", alpha=0.5)
-plt.scatter(centroids_betas_average[:, 0], centroids_betas_average[:, 1], c='blue', marker='x')
+plt.scatter(standard['Margin'], standard['Average Choice'], c= kmeans_margin_average_st.labels_, cmap = "Set1", alpha=0.5)
+plt.scatter(centroids_betas_average_st[:, 0], centroids_betas_average_st[:, 1], c='blue', marker='x')
 plt.title('K-Means cluster for all Subjects - Margin Vs Average Choice')
 plt.xlabel('Margin')
 plt.ylabel('Average Choice')
 plt.show()
 
 
-# In[82]:
+# In[181]:
 
 
 plt.figure(figsize=(16,8))
@@ -189,9 +189,3 @@ plt.colorbar()
 # Using our colour bar we can deduce the clusters from what study they are a part of. If we look at the cluster to the left in our k-means scatter plot we can see that a substantial amount of this cluster contains subjects from the Wood et al study. It is interesting to note this had the highest mean average age of any study in the datasets. It also had a large number of participants but looking at the scatter plot very few participants made money over the course of the trials. The majority had an average choice of below 3 and certainly fell into the category of average lower choice and lower financial gain. This study also features heavily in the second cluster (the one most central) and this cluster also contains subjects who struggled to break even. The Horstmann study also features heavily in this cluster as does the Worthy study in yellow. The Worthy study leans more towards the first cluster again in the lower choice average, lower money made category. It is interesting to note that this study does not explicitly state the age demography of the group studied but tells us it was a solely female, undergraduate student study, which hints at it being a younger age group. In the third cluster to the right, which is the higher average choice, higher profit group we can see a large mix of groups with comparatively less subjects in this cluster compared to the other two. We can see a significant amount of subjects from the Maia study and also quite a few from the previously mentioned Horstmann study. We also see even with a small sample size from the study there is a significant number of Premkumar participants in this profitable cluster. Two of these studies contain a very young mean age again. The Maia study is another that focuses on undergraduate students again, but with better results than previous. 
 
 # Looking at this graph we can see how these studies was very profitable with it clear to see from the scatter plot the majority broke even or made money. We can also easily tell which study made more money than the other from this scatter plot too with the vast majority of the Wetzels studies equal or above a profit margin of 0 dollars. The cluster primarily consists of Steingroever2011's subjects which confirms our earlier analysis that this group made little over half the profit of Wetzels study. The central cluster is much more even, with possibly a few more from the Wetzels study than the Steingroever study. Again, the right most cluster has a fairly even split between the two studies with Wetzels subjects claiming some larger profit margins in contrast to the Steingroever study. It is interesting to note both studies had two cards with a payoff in the decks and both appeared to use similiar age demographies (Wetzels used students, Steingroever a mean average age of 19.9 years old). The larger number of subjects used in the Steingroever study could be attributed to why the winnings were larger in the Wetzels study as boh studies had 2 cards that paid out. Definitely from looking at this graph and our other clusters younger age profiles, particularly students, seem to be more profitable in this task. We also can see that an average card choice of between 3 and 3.25 appears most profitable from our 3 cluster analysis. Higher values closer to 4 also pay well but not as well as between the aforementioned range, while an average value of under 2.5 is detrimental to participants.
-
-# In[ ]:
-
-
-
-
--- _build/jupyter_execute/k-means_variation.py
+++ _build/jupyter_execute/k-means_variation.py
@@ -18,7 +18,7 @@ from sklearn.metrics import silhouette_score
 
 # Import our data
 
-# In[6]:
+# In[65]:
 
 
 index95 = pd.read_csv('data/index_95.csv')
@@ -38,16 +38,51 @@ cleaned95 = pd.read_csv('data/cleaned95.csv', index_col='Unnamed: 0')
 cleaned100 = pd.read_csv('data/cleaned100.csv', index_col='Unnamed: 0')
 cleaned150 = pd.read_csv('data/cleaned150.csv', index_col='Unnamed: 0')
 joined = pd.read_csv('data/cleaned_all.csv', index_col='Unnamed: 0')
+standard = pd.read_csv('data/standardized_all.csv', index_col='Unnamed: 0')
 
 
-# In[7]:
+# In[66]:
 
 
-joined.head()
+standard.head()
+
+
+# In[63]:
+
+
+joined.head(6)
 
 
 # ## Methodology
-# We are going to attempt to follow the methods stated in {cite:p}`LinPP` in his attempt at constructing a privacy preserving clustering technique based on the k-means algorithm. 
+# We are going to attempt to follow the methods stated in {cite:p}`LinPP` in his attempt at constructing a privacy preserving clustering technique based on the k-means algorithm. This involves a 2 step process which is as follows:
+# 
+# 1. Data Protection Phase and
+# 2. Data Recovery Phase
+# 
+# The first phase involving the data protection phase involves 4 key steps. Firstly, we apply the K-means algorithm on our dataset and then we select one of the clusters from the result. In our cluster let's say A, we select the furthest data point away from the centroid of A. We generate the set of noises by the using the following equation:
+# 
+# $$
+#  n_i^u = d^{u} + \alpha \times (distance(c,d))
+# $$
+
+# In[67]:
+
+
+kmeans_margin_joined = KMeans(n_clusters=3).fit(standard[["Margin", "Most Common Choice Picked"]])
+centroids_betas_joined = kmeans_margin_joined.cluster_centers_
+
+
+# In[68]:
+
+
+plt.figure(figsize=(16,8))
+plt.scatter(standard['Margin'], standard['Most Common Choice Picked'], c= kmeans_margin_joined.labels_, cmap = "Set1", alpha=0.5)
+plt.scatter(centroids_betas_joined[:, 0], centroids_betas_joined[:, 1], c='blue', marker='x')
+plt.title('K-Means cluster for all Subjects - Most Common Choice')
+plt.xlabel('Margin')
+plt.ylabel('Times Most Common Choice Picked')
+plt.show()
+
 
 # In[ ]:
 
