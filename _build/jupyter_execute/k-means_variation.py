2184074812b88b7ce660692ed2fc8f63f95f9b6b
Kian Sweeney <73000700+kiansweeney11@users.noreply.github.com>
2021-10-11 15:36:34 +0100

    zbib

--- _build/jupyter_execute/k-means_variation.py
+++ _build/jupyter_execute/k-means_variation.py
@@ -16,7 +16,7 @@ from sklearn.metrics import silhouette_score
 
 # Import our data
 
-# In[65]:
+# In[3]:
 
 
 cleaned95 = pd.read_csv('data/cleaned95.csv', index_col='Unnamed: 0')
@@ -26,7 +26,7 @@ joined = pd.read_csv('data/cleaned_all.csv', index_col='Unnamed: 0')
 standard = pd.read_csv('data/standardized_all.csv', index_col='Unnamed: 0')
 
 
-# In[66]:
+# In[4]:
 
 
 standard.head()
@@ -52,14 +52,14 @@ standard.head()
 # 
 # This is to obtain the position of the noise from eq(1) in dataset D. This leads us on to our data recovery phase. Our first step in the phase is to use eq(2) and obtain p_i for position of noise in D' and commence removals. Then we delete all the noises and the original dataset D can be recovered immediately. The end result should be a dataset that shares cluster information but protects the privacy of the individuals at hand.
 
-# In[69]:
+# In[5]:
 
 
 kmeans_margin_joined = KMeans(n_clusters=3).fit(standard[["Margin", "Most Common Choice Picked"]])
 centroids_betas_joined = kmeans_margin_joined.cluster_centers_
 
 
-# In[70]:
+# In[6]:
 
 
 plt.figure(figsize=(16,8))
@@ -71,79 +71,96 @@ plt.ylabel('Times Most Common Choice Picked')
 plt.show()
 
 
-# In[105]:
+# In[7]:
 
 
 centroids_betas_joined
 
 
-# We can tell from our above cluster centres that cluster 0 is in red, cluster 1 is the centroid of the orange cluster and cluster 2 is in grey.
+# We can tell from our above cluster centres that cluster 0 is in red, cluster 1 is the right most cluster and cluster 2 is in grey.
 
-# In[110]:
+# In[8]:
 
 
 standard['cluster'] = kmeans_margin_joined.labels_.tolist()
 standard.head()
 
 
-# In[174]:
+# In[9]:
 
 
-cluster2 = standard[standard.cluster==2]
-cluster2.head()
+cluster0 = standard[standard.cluster==0]
+cluster0.head()
 
 
-# From looking at our graph and cluster 2, I feel data points with margin values less than -2 would be classified as noise.
+# From looking at our graph and cluster 0, I feel data points with margin values less than -2 would be classified as noise.
 
-# In[176]:
+# In[10]:
 
 
-noises = cluster2[cluster2.Margin <= -2]
+noises = cluster0[cluster0.Margin <= -2]
 noises
 
 
-# In[185]:
-
-
-centre2 = centroids_betas_joined[2]
-xcentre = centre2[0]
-ycentre = centre2[1]
-
-
-# In[192]:
+# In[12]:
 
 
 marginnoise = pd.DataFrame(np.random.uniform(-3.4,-2.5,15))
 
 
-# In[197]:
+# In[13]:
 
 
 marginnoise = marginnoise.rename(columns={0: 'Margin'})
 marginnoise.head()
 
 
-# In[201]:
+# In[14]:
 
 
 choicenoise = pd.DataFrame(np.random.uniform(-0.1,4,15))
-choicenoise.head()
 
 
-# In[202]:
+# In[15]:
 
 
 choicenoise = choicenoise.rename(columns={0: 'Most Common Choice Picked'})
 choicenoise.head()
 
 
-# In[207]:
+# In[16]:
 
 
 noise = pd.concat([marginnoise, choicenoise], axis=1)
 
 
 # Our noise data is now generated we add this back to the original dataset now.
+
+# In[39]:
+
+
+noisesf = noises[['Margin', 'Most Common Choice Picked']]
+noisesf = noisesf.to_numpy()
+noisesf
+
+
+# In[43]:
+
+
+centroids_betas_joined[0]
+
+
+# In[46]:
+
+
+from scipy.spatial.distance import cdist
+from scipy.spatial import distance
+
+distances = distance.cdist(centroids_betas_joined, noisesf, 'euclidean')
+distances[0]
+
+
+# Our furtherest point away from the centroid of our choosen cluster 0 is the first point we see in the array here. This will be denoted as our data "d" the furtherest point from our centroid, C of cluster 0.
 
 # In[ ]:
 
