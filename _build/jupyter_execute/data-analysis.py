9268ff05dbf5ecfecc9d2a5989087e3faef40124
Kian Sweeney <73000700+kiansweeney11@users.noreply.github.com>
2021-10-12 19:18:31 +0100

    meta-intro

--- _build/jupyter_execute/data-analysis.py
+++ _build/jupyter_execute/data-analysis.py
@@ -30,7 +30,7 @@ index150 = pd.read_csv('data/index_150.csv')
 
 # Let's just confirm our python version we are using on our scripts
 
-# In[55]:
+# In[4]:
 
 
 from platform import python_version
@@ -41,7 +41,7 @@ print(python_version())
 
 # Here we check the study that used 95 trials of the experiment and see how many of the 15 subjects made profit.
 
-# In[4]:
+# In[5]:
 
 
 win95 = pd.read_csv('data/wi_95.csv')
@@ -50,21 +50,21 @@ totalloss95 = loss95.sum(axis=1)
 totalloss95.head()
 
 
-# In[5]:
+# In[6]:
 
 
 totalwin95 = win95.sum(axis=1)
 totalwin95.head()
 
 
-# In[6]:
+# In[7]:
 
 
 margin95 = totalwin95 + totalloss95
 margin95.head()
 
 
-# In[7]:
+# In[8]:
 
 
 columns = ['Margin']
@@ -75,7 +75,7 @@ ax1 = margin95df.plot.kde(title='Density plot of Profit/Loss margin for 95 peopl
 ax1.axvline(x=0, linestyle='--', color='red')
 
 
-# In[8]:
+# In[9]:
 
 
 sum(margin95df.select_dtypes(np.number).gt(0).sum(axis=1))
@@ -83,7 +83,7 @@ sum(margin95df.select_dtypes(np.number).gt(0).sum(axis=1))
 
 # Under half (7/15) of the participants made profit in the 95 trial experiment. We now do the same for both the 100 trial and 150 trial experiments
 
-# In[9]:
+# In[10]:
 
 
 win100 = pd.read_csv('data/wi_100.csv')
@@ -92,21 +92,21 @@ totalloss100 = loss100.sum(axis=1)
 totalloss100.head()
 
 
-# In[10]:
+# In[11]:
 
 
 totalwin100 = win100.sum(axis=1)
 totalwin100.head()
 
 
-# In[11]:
+# In[12]:
 
 
 margin100 = totalwin100 + totalloss100
 margin100.head()
 
 
-# In[12]:
+# In[13]:
 
 
 columns = ['Margin']
@@ -117,7 +117,7 @@ ax2 = margin100df.plot.kde(title='Density plot of Profit/Loss margin for 100 peo
 ax2.axvline(x=0, linestyle='--', color='red')
 
 
-# In[13]:
+# In[14]:
 
 
 sum(margin100df.select_dtypes(np.number).gt(0).sum(axis=1))
@@ -125,7 +125,7 @@ sum(margin100df.select_dtypes(np.number).gt(0).sum(axis=1))
 
 # Only 41% of participants in the 100 trial experiment made money!
 
-# In[14]:
+# In[15]:
 
 
 win150 = pd.read_csv('data/wi_150.csv')
@@ -134,21 +134,21 @@ totalloss150 = loss150.sum(axis=1)
 totalloss150.head()
 
 
-# In[15]:
+# In[16]:
 
 
 totalwin150 = win150.sum(axis=1)
 totalwin150.head()
 
 
-# In[16]:
+# In[17]:
 
 
 margin150 = totalwin150 + totalloss150
 margin150.head()
 
 
-# In[17]:
+# In[18]:
 
 
 columns = ['Margin']
@@ -159,29 +159,29 @@ ax = margin150df.plot.kde(title='Density plot of Profit/Loss margin for 150 peop
 ax.axvline(x=0, linestyle='--', color='red')
 
 
-# In[18]:
+# In[19]:
 
 
 sum(margin150df.select_dtypes(np.number).gt(0).sum(axis=1))
 
 
-# There is a much higher % of people making profit in the 150 trial experiment (63%). There is far less people taking part in this experiment than the 100 trial experiment but it may be worth drilling down further into the data to check the differences between the different groups undertaking the experiments here. It might be interesting to see if there is any trends surrounding the age profiles surveyed. We will now try to combine the study groups with our "margin" results.
+# There is a much higher % of people making profit in the 150 trial experiment (63%). There is far less people taking part in this experiment than the 100 trial experiment but it may be worth drilling down further into the data to check the differences between the different groups undertaking the experiments here. It might be interesting to see if there is any trends surrounding the age or the gender of the subjects or the number of cards that paid out in the study in question. We will now try to combine the study groups with our "margin" results.
 
 # ### Adding in Study undertakers to participant data
 
-# In[21]:
+# In[20]:
 
 
 margin95df['Study'] = index95['Study'].values
 
 
-# In[24]:
+# In[21]:
 
 
 margin150df['Study'] = index150['Study'].values
 
 
-# In[26]:
+# In[22]:
 
 
 margin100df['Study'] = index100['Study'].values
@@ -189,20 +189,20 @@ margin100df['Study'] = index100['Study'].values
 
 # Here we will investigate the Study groups in the 150 person experiment. We will try to see does one group achieve better results in the task than the other.
 
-# In[28]:
+# In[23]:
 
 
 margin150df['Study'].value_counts()
 
 
-# In[29]:
+# In[24]:
 
 
 print("Profit for the 150 trial Wetzels study")
 margin150df.loc[margin150df['Study'] == 'Wetzels', 'Margin'].sum()
 
 
-# In[30]:
+# In[25]:
 
 
 print("Profit for the 150 trial Steingroever2011 study")
@@ -210,57 +210,57 @@ margin150df.loc[margin150df['Study'] == 'Steingroever2011', 'Margin'].sum()
 
 
 # ### Assessment of 150 margin results for each study
-# This is interesting to note these values. Both studies make a large cumulative profit over the course of the 150 trials undertaken. There is a net profit between the two studies of 36,550 dollars, almost an average profit of 375 dollars per participant. It is interesting to note that the 41 students in "Wetzels" study were exclusively students, while in Steingroever2011's study there was a young average age (19.9) but no specific mention of if the participants were students or not. Although they made money it was almost half of the other group studied. We will measure this against the other datasets to see if age is a factor between the decision making of the groups.
+# This is interesting to note these values. Both studies make a large cumulative profit over the course of the 150 trials undertaken. There is a net profit between the two studies of 36,550 dollars, almost an average profit of 375 dollars per participant. It is interesting to note that the 41 students in "Wetzels" study were exclusively students, while in Steingroever2011's study there was a young average age (19.9) but no specific mention of if the participants were students or not. Although they made money it was almost half of the other group studied. We will now measure this against other datasets.
 
-# In[31]:
+# In[26]:
 
 
 margin100df['Study'].value_counts()
 
 
-# In[32]:
+# In[27]:
 
 
 print("Loss for the 100 trial Horstmann study")
 margin100df.loc[margin100df['Study'] == 'Horstmann', 'Margin'].sum()
 
 
-# In[33]:
+# In[28]:
 
 
 print("Loss for the 100 trial Wood study")
 margin100df.loc[margin100df['Study'] == 'Wood', 'Margin'].sum()
 
 
-# In[34]:
+# In[29]:
 
 
 print("Loss for the 100 trial SteingroverInPrep study")
 margin100df.loc[margin100df['Study'] == 'SteingroverInPrep', 'Margin'].sum()
 
 
-# In[35]:
+# In[30]:
 
 
 print("Profit for the 100 trial Maia study")
 margin100df.loc[margin100df['Study'] == 'Maia', 'Margin'].sum()
 
 
-# In[36]:
+# In[31]:
 
 
 print("Loss for the 100 trial Worthy study")
 margin100df.loc[margin100df['Study'] == 'Worthy', 'Margin'].sum()
 
 
-# In[37]:
+# In[32]:
 
 
 print("Profit for the 100 trial Premkumar study")
 margin100df.loc[margin100df['Study'] == 'Premkumar', 'Margin'].sum()
 
 
-# In[38]:
+# In[33]:
 
 
 print("Loss for the 100 trial Kjome study")
@@ -268,15 +268,15 @@ margin100df.loc[margin100df['Study'] == 'Kjome', 'Margin'].sum()
 
 
 # ### Assessment of margin for 100 trial experiments
-# The results here are in stark contrast to the 150 trial experiments. Despite less trials there is some significant losses accumalated by participants. Although the study conducted by Wood has a large number of participants in 153, the loss of 119,410 is certainly a major outlier. This equates to an average loss of roughly 780 dollars per person. It is particularly interesting to note [here](https://openpsychologydata.metajnl.com/articles/10.5334/jopd.ak/) in table 1, we see this group has the oldest average age of any group in the study by some distance. The next oldest average age specified actually makes a profit (Premkumar). Again we see students with strong results in the Maia study as undergraduate students here make a strong profit, similar to the groups in the 150 trial experiments. We now check the 95 trial study as our last part of our margin analysis. 
+# The results here are in stark contrast to the 150 trial experiments. Despite less trials there is some significant losses accumalated by participants. Although the study conducted by Wood has a large number of participants in 153, the loss of 119,410 is certainly a major outlier. This equates to an average loss of roughly 780 dollars per person. It is particularly interesting to note [here](https://openpsychologydata.metajnl.com/articles/10.5334/jopd.ak/) in table 1, we see this group has the oldest average age of any group in the study by some distance. However, there is a significant cohort of this group (the first 90) who are between 18-40 years old. The next oldest average age specified actually makes a profit (Premkumar). This potentially suggests age might not be a major factor. Again we see students with strong results in the Maia study as undergraduate students here make a strong profit, similar to the groups in the 150 trial experiments. We now check the 95 trial study as our last part of our margin analysis. 
 
-# In[39]:
+# In[34]:
 
 
 margin95df.head()
 
 
-# In[40]:
+# In[35]:
 
 
 print("Profit for the 95 trial Fridberg study")
@@ -290,7 +290,7 @@ margin95df.loc[margin95df['Study'] == 'Fridberg', 'Margin'].sum()
 
 # We will start by looking at the participants in the 95 trial experiment. To merge our dataframes together we will need to change the column names so that they share common names.
 
-# In[41]:
+# In[36]:
 
 
 columnnames95 = [f'Trial{num}' for num in range(1,96)]
@@ -298,14 +298,14 @@ wins95test = win95
 wins95test = wins95test.set_axis(columnnames95, axis=1)
 
 
-# In[42]:
+# In[37]:
 
 
 loss95test = loss95
 loss95test = loss95test.set_axis(columnnames95, axis=1)
 
 
-# In[43]:
+# In[38]:
 
 
 df95_added = wins95test.add(loss95test, fill_value=0)
@@ -313,56 +313,54 @@ df95_added = wins95test.add(loss95test, fill_value=0)
 
 # This study was all part of the Fridberg study so we don't need to worry about comparing other studies here.
 
-# In[44]:
+# In[39]:
 
 
 per_trial_95 = df95_added.sum(axis=0)
 
 
-# In[45]:
+# In[62]:
 
 
-per_trial_95.plot(title='Fridberg Study Win/Loss per round', color='green')
+per_trial_95.plot(title='Fridberg Study Win/Loss per round', color='green', figsize=(14,8))
 
 
 # ### Next we move onto the 150 trial experiments and see how these studies flowed over the course of their trials
 
-# In[46]:
+# In[41]:
 
 
 columnnames150 = [f'Trial{num}' for num in range(1,151)]
-#columnnames95
 wins150test = win150
-#wins95test.head()
 wins150test = wins150test.set_axis(columnnames150, axis=1)
 
 
-# In[47]:
+# In[42]:
 
 
 loss150test = loss150
 loss150test = loss150test.set_axis(columnnames150, axis=1)
 
 
-# In[48]:
+# In[43]:
 
 
 loss150test.head()
 
 
-# In[49]:
+# In[44]:
 
 
 df150_added = wins150test.add(loss150test, fill_value=0)
 
 
-# In[50]:
+# In[45]:
 
 
 df150_added['Study'] = index150['Study'].values
 
 
-# In[51]:
+# In[46]:
 
 
 teststein = df150_added.loc[df150_added['Study'] == 'Steingroever2011']
@@ -370,13 +368,13 @@ del teststein['Study']
 per_trial_150_stein = teststein.sum(axis=0)
 
 
-# In[52]:
+# In[61]:
 
 
-per_trial_150_stein.plot(title="Winnings per round for Steingroever2011's study", color="green")
+per_trial_150_stein.plot(title="Winnings per round for Steingroever2011's study", color="green", figsize=(14,8))
 
 
-# In[53]:
+# In[48]:
 
 
 testWetzels = df150_added.loc[df150_added['Study'] == 'Wetzels']
@@ -384,10 +382,89 @@ del testWetzels['Study']
 per_trial_150_wetzels = testWetzels.sum(axis=0)
 
 
-# In[54]:
+# In[60]:
 
 
-per_trial_150_wetzels.plot(title="Winnings per round for Wetzels study", color="green")
+per_trial_150_wetzels.plot(title="Winnings per round for Wetzels study", color="green", figsize=(14,8))
 
 
 # After assessing the participants winnings in each study and how their respective win / losses flowed per trial I decided to delve deeper into why these studies appeared more profitable than others. It is easy to point to the varying amounts of cards that pay out on each study and the number of respective trials each study undertook or the number of participants each study had. I felt it would be interesting to build on the win / loss flow per trials mentioned just above and try to pick out patterns accordingly. This would be something like if studies lost a lot of money is this down to a lower average choice of deck or constant changing of choosen cards and see if we could link this to research and studies on the IGT. This study and research could be related to gender based decision making or age demographies and see if these related studies findings hold true to our sample data.
+
+# ### Lastly, we look at our 100 Trial studies selection flow
+
+# In[50]:
+
+
+columnnames100 = [f'Trial{num}' for num in range(1,101)]
+wins100test = win100
+wins100test = wins100test.set_axis(columnnames100, axis=1)
+loss100test = loss100
+loss100test = loss100test.set_axis(columnnames100, axis=1)
+df100_added = wins100test.add(loss100test, fill_value=0)
+df100_added['Study'] = index100['Study'].values
+
+
+# In[59]:
+
+
+testin = df100_added.loc[df100_added['Study'] == 'SteingroverInPrep']
+del testin['Study']
+per_trial_100_in = testin.sum(axis=0)
+per_trial_100_in.plot(title="Winnings per round for SteingroverInPrep's study", color="green", figsize=(14,8))
+
+
+# In[63]:
+
+
+testworth = df100_added.loc[df100_added['Study'] == 'Worthy']
+del testworth['Study']
+per_trial_100_wor = testworth.sum(axis=0)
+per_trial_100_wor.plot(title="Winnings per round for Worthy's study",color="green", figsize=(14,8))
+
+
+# In[64]:
+
+
+testwood = df100_added.loc[df100_added['Study'] == 'Wood']
+del testwood['Study']
+per_trial_100_wood = testwood.sum(axis=0)
+per_trial_100_wood.plot(title="Winnings per round for Wood's study",color="green", figsize=(14,8))
+
+
+# In[67]:
+
+
+testmaia = df100_added.loc[df100_added['Study'] == 'Maia']
+del testmaia['Study']
+per_trial_100_maia = testmaia.sum(axis=0)
+per_trial_100_maia.plot(title="Winnings per round for Maia's study",color="green", figsize=(14,8))
+
+
+# In[68]:
+
+
+testhorstmann = df100_added.loc[df100_added['Study'] == 'Horstmann']
+del testhorstmann['Study']
+per_trial_100_horstmann = testhorstmann.sum(axis=0)
+per_trial_100_horstmann.plot(title="Winnings per round for Horstmann's study",color="green", figsize=(14,8))
+
+
+# In[70]:
+
+
+testprekumar = df100_added.loc[df100_added['Study'] == 'Premkumar']
+del testprekumar['Study']
+per_trial_100_prekumar = testprekumar.sum(axis=0)
+per_trial_100_prekumar.plot(title="Winnings per round for Prekumar's study",color="green", figsize=(14,8))
+
+
+# In[71]:
+
+
+testkjome = df100_added.loc[df100_added['Study'] == 'Kjome']
+del testkjome['Study']
+per_trial_100_kjome = testkjome.sum(axis=0)
+per_trial_100_kjome.plot(title="Winnings per round for Kjome's study",color="green", figsize=(14,8))
+
+
+# Looking at our selection flow graphs we can see studies that lose significant amounts of money such as the Wood and Worthy study start well for the initial trials, all in good profit but big dips around the 21st trial mark for both mark a downturn in results. This points to a varied selection policy in that even after exploring the decks at the start and seeing that they win the subjects deviate from these and pay the price with losses building up. The more profitable Maia and Premkumar studies are noticeable in that whenever they encounter a large dip they normally recover quickly and don't lose as big for many trials again. This points to a more informed knowledge of the decks and not being unsettled by such losses. We will now use our information gathered here to break down the studies and detect trends in age, gender number of trials / participants and so forth. To do this we will need to process the given data accordingly.
